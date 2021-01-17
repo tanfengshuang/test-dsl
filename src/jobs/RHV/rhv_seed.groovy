3c8597b068e7b23867cb731592452c571be31f88
Fengshuang Tan <ftan@redhat.com>
2021-01-17 10:37:02 +0800

    u

--- src/jobs/RHV/rhv_seed.groovy
+++ src/jobs/RHV/rhv_seed.groovy
@@ -29,32 +29,31 @@ jobConfig.each { jobName, config ->
             maxPerNode(3)
             maxTotal(0)
         }
-        if(jobName.contains("rhv_server_x86_64")){
-            parameters {
-                string(name: 'ERRATA_ID', defaultValue: '', description: 'Set ERRATA_ID or Manifest_URL for manual trigger. Manifest_URL will take affect if both are set.')
-                string(name: 'Manifest_URL', defaultValue: '', description: 'Set ERRATA_ID or Manifest_URL for manual trigger. Manifest_URL will take affect if both are set.')
-                string(name: 'Distro', defaultValue: 'RHEL-7.9-updates-20201102.0', description: 'Fill in RHEL-H Distro here, such as RHEL-7.7.')
-                string(name: 'PID', defaultValue: '', description: 'Set RHV product ids listed in package manifest, RHV PIDs on Server_x86_64 - 69, 150, 415, 421, 479.')
-                choice(name: 'Variant', choices: 'Server\nBaseOS', description: 'Select Variant.')
-                choice(name: 'Arch', choices: 'x86_64\n', description: 'Select Arch.')
-                choice(name: 'CDN', choices: 'Stage\nProd', description: 'Select CDN, Stage or Prod.')
-                choice(name: 'Candlepin', choices: 'Stage\nProd', description: 'Select Candlepin, Stage or Prod.')
-                string(name: 'Testing_System', defaultValue: '', description: 'Optional, fill in the hostname or IP of one system which has correct RHV version and arch here, then will do testing on it rather than provison beaker system.')
-                string(name: 'Password', defaultValue: 'QwAo2U6GRxyNPKiZaOCx', description: "Optional, used for Testing_System, the default value above is beaker's default password, please modify it if needed, but please make sure correct password here.")
+
+        parameters {
+            if(jobName.contains("rhv_server_x86_64")){
+                stringParam('ERRATA_ID', '', 'Set ERRATA_ID or Manifest_URL for manual trigger. Manifest_URL will take affect if both are set.')
+                stringParam('Manifest_URL', '', 'Set ERRATA_ID or Manifest_URL for manual trigger. Manifest_URL will take affect if both are set.')
+                stringParam('Distro', 'RHEL-7.9-updates-20201102.0', 'Fill in RHEL-H Distro here, such as RHEL-7.7.')
+                stringParam('PID', '', 'Set RHV product ids listed in package manifest, RHV PIDs on Server_x86_64 - 69, 150, 415, 421, 479.')
+                choiceParam('Variant', 'Server\nBaseOS', 'Select Variant.')
+                choiceParam('Arch', 'x86_64\n', 'Select Arch.')
+                choiceParam('CDN', 'Stage\nProd', 'Select CDN, Stage or Prod.')
+                choiceParam('Candlepin', 'Stage\nProd', 'Select Candlepin, Stage or Prod.')
+                stringParam('Testing_System', '', 'Optional, fill in the hostname or IP of one system which has correct RHV version and arch here, then will do testing on it rather than provison beaker system.')
+                stringParam('Password', 'QwAo2U6GRxyNPKiZaOCx', "Optional, used for Testing_System, the default value above is beaker's default password, please modify it if needed, but please make sure correct password here.")
             }
-        }
-        if(jobName.contains("rhv_rhvh_x86_64")){
-            parameters {
-                string(name: 'ERRATA_ID', defaultValue: '', description: 'Set ERRATA_ID or Manifest_URL for manual trigger. Manifest_URL will take affect if both are set.')
-                string(name: 'Manifest_URL', defaultValue: '', description: 'Set ERRATA_ID or Manifest_URL for manual trigger. Manifest_URL will take affect if both are set.')
-                string(name: 'Distro', defaultValue: 'RHEL-7.9-updates-20201102.0', description: 'Fill in RHEL-H Distro here, such as RHEL-7.7.')
-                string(name: 'PID', defaultValue: '', description: 'Set RHV product ids listed in package manifest, RHV PIDs on Server_x86_64 - 69, 150, 415, 421, 479.')
-                choice(name: 'Variant', choices: 'Server\nBaseOS', description: 'Select Variant.')
-                choice(name: 'Arch', choices: 'x86_64\n', description: 'Select Arch.')
-                choice(name: 'CDN', choices: 'Stage\nProd', description: 'Select CDN, Stage or Prod.')
-                choice(name: 'Candlepin', choices: 'Stage\nProd', description: 'Select Candlepin, Stage or Prod.')
-                string(name: 'Testing_System', defaultValue: '', description: 'Optional, fill in the hostname or IP of one system which has correct RHV version and arch here, then will do testing on it rather than provison beaker system.')
-                string(name: 'Password', defaultValue: 'QwAo2U6GRxyNPKiZaOCx', description: "Optional, used for Testing_System, the default value above is beaker's default password, please modify it if needed, but please make sure correct password here.")
+            if(jobName.contains("rhv_rhvh_x86_64")){
+                stringParam('ERRATA_ID', '', 'Set ERRATA_ID or Manifest_URL for manual trigger. Manifest_URL will take affect if both are set.')
+                stringParam('Manifest_URL', '', 'Set ERRATA_ID or Manifest_URL for manual trigger. Manifest_URL will take affect if both are set.')
+                stringParam('Distro', 'RHEL-7.9-updates-20201102.0', 'Fill in RHEL-H Distro here, such as RHEL-7.7.')
+                stringParam('PID', '', 'Set RHV product ids listed in package manifest, RHV PIDs on Server_x86_64 - 69, 150, 415, 421, 479.')
+                choiceParam('Variant', 'Server\nBaseOS', 'Select Variant.')
+                choiceParam('Arch', 'x86_64\n', 'Select Arch.')
+                choiceParam('CDN', 'Stage\nProd', 'Select CDN, Stage or Prod.')
+                choiceParam('Candlepin', 'Stage\nProd', 'Select Candlepin, Stage or Prod.')
+                stringParam('Testing_System', '', 'Optional, fill in the hostname or IP of one system which has correct RHV version and arch here, then will do testing on it rather than provison beaker system.')
+                stringParam('Password', 'QwAo2U6GRxyNPKiZaOCx', "Optional, used for Testing_System, the default value above is beaker's default password, please modify it if needed, but please make sure correct password here.")
             }
         }
         definition {
